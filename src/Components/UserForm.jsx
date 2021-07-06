7d6274762dc82cb7df9a4e143b8d653e218f3a31
Bilal Zak <bzak954@gmail.com>
2021-07-06 14:23:48 +0000

    add userform

---
+++ src/Components/UserForm.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class UserForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+             name : '',
+             email : '',
+             gen : ''
+        }
+    }
+
+
+    handleChange = (e)=>{
+        this.setState({ [e.target.name] : e.target.value})
+    }
+
+    handleSubmit = (e)=>{
+        e.preventDefault()
+        this.props.addUser(this.state)
+
+
+        this.setState({
+            name : '',
+            email : '',
+            gen : ''
+        })
+
+        
+    }
+
+
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+               <div className="name">
+                  <label htmlFor="name">Name</label><br/>
+                  <input type="text" name="name" value={this.state.name} 
+                  onChange={this.handleChange} placeholder="User Name"/>
+               </div>
+               <div className="email">
+                  <label htmlFor="name">Email</label><br/>
+                  <input type="email" name="email" value={this.state.email}
+                   onChange={this.handleChange} placeholder="Example@email.com"/>
+               </div>
+               <div className="gen">
+                  <label htmlFor="name">Gen</label><br/>
+                  <input type="number" name="gen" value={this.state.gen}
+                   onChange={this.handleChange} placeholder="Gen"/>
+               </div>
+               <button type="submit">Add User</button>
+            </form>
+        );
+    }
+}
+
+export default UserForm;
