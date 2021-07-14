4ca0b6b2487eb3d2cdad321aa676283cb1e7b577
Bilal Zak <bzak954@gmail.com>
2021-07-14 21:33:50 +0000

    add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UserForm from './Components/UserForm'
+import UserList from './Components/UserList'
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { users :[]}
+    }
+
+    handleAddUser = (newUser) => {
+        this.setState({ 
+            users: [...this.state.users,newUser] 
+        })
+    }
+
+    render() {
+        
+       
+
+        return (
+            <> 
+                <UserForm addUser={this.handleAddUser}/>
+                <h1 style={{textAlign:'center',marginTop:'50px'}}>Added Users</h1><hr/>
+                <UserList  users= {this.state.users}/>
+            </>
+        );
+    }
 }
 
 export default App;
