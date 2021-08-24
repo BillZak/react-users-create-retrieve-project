63165ecf3685d3c5f607d8d065ea8584c1772916
Bill Zack <bzak954@gmail.com>
2021-08-24 12:58:48 +0000

    add action states

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,6 +1,21 @@
 export const addUser =  ( newUser )=>{
+    newUser.id = Math.random().toString();
      return{
          type: "ADD_USER",
          payload: newUser,
      }
+}
+
+export const editUser = ( updatedUser )=>{
+    return{
+        type: "EDIT_USER",
+        payload: updatedUser,
+    }
+}
+
+export const deleteUser = (userId)=>{
+    return{
+       type: "DELETE_USER",
+       payload: userId,
+    }
 }
