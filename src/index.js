784ee5cb3fec8ef7e19ce151047ae72d74dbbf73
Bill Zack <bzak954@gmail.com>
2021-09-14 13:39:04 +0000

    add fragments to cover the app

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import {Provider} from 'react-redux'
 
 ReactDOM.render(
   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <>
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>,
+   </>
   </Provider>,
   document.getElementById('root')
 );
