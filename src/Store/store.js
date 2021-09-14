92402a854602c4088ceb33cb14d134f3db63aed6
Bill Zack <bzak954@gmail.com>
2021-09-14 13:40:04 +0000

    add firebase as argument to the reduxFirestore

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,7 +1,7 @@
 import { createStore , compose , applyMiddleware } from "redux";
 import usersReducer from "../Reducers/usersReducer";
-import { getFirebase, reactReduxFirebase } from "react-redux-firebase";
 import { getFirestore, reduxFirestore } from "redux-firestore";
+import { getFirebase, reactReduxFirebase } from "react-redux-firebase";
 import  thunk  from "redux-thunk"
 import  firebase  from '../firebase/config'
 
@@ -9,7 +9,7 @@ const store = createStore( usersReducer,
     compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore })),
     reactReduxFirebase(firebase),
-    reduxFirestore(getFirestore),
+    reduxFirestore(firebase),
 )  )
 
 
