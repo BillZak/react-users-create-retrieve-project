cb747ab37cc1eb2728013d1e1d7261e53243a566
Bill Zack <bzak954@gmail.com>
2021-09-14 13:39:30 +0000

    add function to connect the database

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,9 +1,18 @@
-export const addUser =  ( newUser )=>{
-    newUser.id = Math.random().toString();
-     return{
-         type: "ADD_USER",
-         payload: newUser,
-     }
+import { getFirestore } from "redux-firestore"
+
+export const addUser =  ( newUser ) => {
+   // newUser.id = Math.random().toString();
+    return(dispatch, state, {getFirestore})=>{
+      getFirestore()
+      .collection('users')
+      .add(newUser).then((docs)=>{
+          console.log(docs);
+      });
+    };
+    // return{
+     //    type: "ADD_USER",
+      //   payload: newUser,
+    // }
 }
 
 export const editUser = ( updatedUser )=>{
