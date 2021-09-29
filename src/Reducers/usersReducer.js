2dc5a30e2b41787ee0b789cc5ffe1beb602217be
Bill Zack <bzak954@gmail.com>
2021-09-29 21:01:16 +0000

    add reducer

--- src/Reducers/usersReducer.js
+++ src/Reducers/usersReducer.js
@@ -1,19 +1,23 @@
 const initialState = {
     users :[]
 };
+
 var usersReducer = ( state = initialState ,action) => {
     switch(action.type){
         case "ADD_USER":
         return {...state.users, users: [...state.users, action.payload]};
+        
         case "DELETE_USER":
             const savedusers = state.users.filter( 
                 (user)=>{
                    return user.id !== action.payload;
                 })
         return {...state,users: savedusers}
+
         case "EDIT_USER":
         return{...state,users: state.users.map((user) =>
             user.id === action.payload.id ? action.payload : user )}
+
         case "SET_ALL_USERS":
         return {...state, users: action.payload}
 
